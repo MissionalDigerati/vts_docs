3f3f0793ebd1b0345ac22ef221479cf622e3c9db
Codemis <johnathan@missionaldigerati.com>
2012-08-22 14:34:26 -0700

    Added documentation folder with default index files.

---
+++ documentation/v1/web_interface/index.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of Video Translator Service Documentation.
+ * 
+ * Video Translator Service Documentation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Video Translator Service Documentation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @copyright Copyright 2012 Missional Digerati
+ * 
+ */
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php 
+			$title = "Redeeming Technology for Kingdom Work";
+			require_once('../../../partials/site_wide_css_js.inc.php'); 
+		?>
+	</head>
+	<body>
+		<div class="navbar">
+	    <div class="navbar-inner">
+	      <div class="container">
+	        	<?php require_once('../../../partials/main_nav.inc.php'); ?>
+	      </div>
+	    </div>
+	  </div>
+		<div class="slider">
+			<div class="inner">
+				<div class="container">
+					<div class="camera_wrap">
+					   <!-- Slideshow Here -->
+					</div>
+				</div>
+			</div>
+		</div>
+	  <div class="content">
+			<div class="inner">
+				<div class="container home">
+					  <div class="padded_content">
+							<!-- Content Here -->
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
