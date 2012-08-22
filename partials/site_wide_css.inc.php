9d752f1c2b95b90bf426c85635f0cdb2c3a4bf3c
Codemis <johnathan@missionaldigerati.com>
2012-08-22 15:53:00 -0700

    Seprated out site wide js and css partials.

---
+++ partials/site_wide_css.inc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of Video Translator Service Documentation.
+ * 
+ * Video Translator Service Documentation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Video Translator Service Documentation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @copyright Copyright 2012 Missional Digerati
+ * 
+ */
+?>
+<title>Missional Digerati:: <?php echo $title; ?></title>
+<link rel="stylesheet" href="/vendor/stylesheets/jquery.camera.css" type="text/css" charset="utf-8">
+<link rel="stylesheet" href="/vendor/stylesheets/jquery.colorbox.css" type="text/css" charset="utf-8">
+<link rel="stylesheet" href="/stylesheets/compiled/custom_bootstrap.css" type="text/css" charset="utf-8">
+<link rel="stylesheet" href="/stylesheets/compiled/application.css" type="text/css" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
---
+++ partials/site_wide_js.inc.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Video Translator Service Documentation.
+ * 
+ * Video Translator Service Documentation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Video Translator Service Documentation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @copyright Copyright 2012 Missional Digerati
+ * 
+ */
+?>
+<!--[if lt IE 9]>
+ <script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+<![endif]-->
+<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="/vendor/bootstrap/javascript/bootstrap.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="/vendor/javascript/jquery.easing.1.3.js"></script>
+<script type="text/javascript" charset="utf-8" src="/vendor/javascript/jquery.camera.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="/vendor/javascript/jquery.colorbox.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="/javascript/application.js"></script>
